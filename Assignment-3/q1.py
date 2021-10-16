488f166173cb1e661a9b9cb2438a9fb5e04ff70f
Arnav Tuli <arnavtuli00@gmail.com>
2021-10-16 20:42:29 +0530

    Implemented basic decision tree

--- Assignment-3/q1.py
+++ Assignment-3/q1.py
@@ -1,6 +1,35 @@
+from numpy.lib.shape_base import split
 import pandas as pd
 import numpy as np
 
+
+# create Tree class (for decision tree nodes)
+class Tree:
+    # constructor
+    def __init__(self, attr=-1, val=-1, children=[]):
+        self.attr = attr
+        self.val = val
+        self.children = children
+    # get methods
+    def getValue(self):
+        return self.val
+    def getAttribute(self):
+        return self.attr
+    def getChildren(self):
+        return self.children
+    # set methods
+    def setValue(self, val):
+        self.val = val
+    def setAttribute(self, attr):
+        self.attr = attr
+    def addChild(self, child):
+        self.children.append(child)
+    # method to check for leaf node
+    def isLeaf(self):
+        if self.children == []:
+            return True
+        else:
+            return False
 
 # function to give coding to categorical data
 # X: numpy array
@@ -182,6 +211,138 @@ def parse_csv(file):
     # return the data (input/output)
     return X, X_hot, Y
 
+# function to split data on a given attribute
+# X: input data
+# Y: output data
+# attr_index: attribute index to split on
+# num_split: whether the attribute to split on is numeric
+# cat_range: range in case of categorical data
+def split_data(X, Y, attr_index, num_split, cat_range=0):
+    # create lists
+    X_list = []
+    Y_list = []
+    if num_split:
+        # binary split
+        # find median value of 'attr_index' attribute
+        median = np.median(X[:, attr_index].reshape(-1))
+        # find examples strictly greater than the median
+        filter = (X[:, attr_index].reshape(-1) > median)
+        # store data with attr_value <= median
+        X_list.append(X[filter==False])
+        Y_list.append(Y[filter==False])
+        # store data with attr_value > median
+        X_list.append(X[filter])
+        Y_list.append(Y[filter])
+    else:
+        # cat_range-way split
+        for i in range(cat_range):
+            # filter examples with attr_value i
+            filter = (X[:, attr_index] == i)
+            # store data with attr_value = i    
+            X_list.append(X[filter])
+            Y_list.append(Y[filter])
+    # return data lists
+    return X_list, Y_list
+
+# function to choose the best attribute to split the data on
+# X: input data
+# Y: output data
+# num_attr: set of attribute indices with numeric values
+# cat_attr: dictionary containing range of categorical attributes
+def best_attribute_split(X, Y, num_attr, cat_attr):
+    # store number of examples and features
+    m, n = X.shape
+    # go through all features and choose the one that maximizes information gain
+    min_entropy = -1
+    min_index = -1
+    for attr_index in range(n):
+        if attr_index in num_attr:
+            # numeric attribute
+            X_list, Y_list = split_data(X, Y, attr_index, True)
+        else:
+            # categorical attribute
+            X_list, Y_list = split_data(X, Y, attr_index, False, cat_attr[attr_index])
+        # determine split entropy
+        entropy = 0
+        for i in range(len(X_list)):
+            # store number of examples
+            temp = X_list[i].shape[0]
+            # compute prob of example having this attr value
+            prob = (temp / m)
+            # check for zero probability
+            if prob == 0:
+                continue
+            # compute H(y|X=x)
+            one_count = np.sum(Y_list[i], axis=0)
+            # check for zero entropy
+            if one_count == 0 or one_count == temp:
+                continue
+            h = (one_count / temp) * np.log(temp / one_count) + (1 - one_count / temp) * np.log(temp / (temp - one_count))
+            # add to entropy
+            entropy += (h * prob)
+        # check for minimum entropy
+        if min_index == -1:
+            min_index = attr_index
+            min_entropy = entropy
+        elif entropy < min_entropy:
+            min_index = attr_index
+            min_entropy = entropy
+    # return attribute that minimizes entropy (maximizes information gain)
+    return min_index
+
+# recursive function used to create the decision tree (binary classification)
+# X: incoming input (training data)
+# Y: incoming output (training data)
+# num_attr: set of attribute indices with numeric values
+# cat_attr: dictionary containing range of categorical attributes
+def decision_tree(X, Y, num_attr, cat_attr):
+    # store number of examples
+    m = X.shape[0]
+    # determine number of examples of class 1
+    checksum = np.sum(Y, axis=0)
+    # return leaf-node with majority class, if data set is empty
+    if m == 0:
+        if checksum > m - checksum:
+            # majority class is '1'
+            node = Tree(-1, 1, [])
+            return node
+        else:
+            # majority class is '0'
+            node = Tree(-1, 0, [])
+            return node
+    # check if data is homogeneous
+    if checksum == 0:
+        # all examples have zero output
+        # return a leaf node (val=0)
+        node = Tree(-1, 0, [])
+        return node
+    elif checksum == m:
+        # all examples have 'one' output
+        # return a leaf node (val=1)
+        node = Tree(-1, 1, [])
+        return node
+    # else, the data is not homogeneous and not empty
+    # create internal node
+    node = Tree()
+    # choose an attribute to split on
+    attr_index = best_attribute_split(X, Y, num_attr, cat_attr)
+    if attr_index in num_attr:
+        # attribute is numeric, perform 2-way split on median
+        X_list, Y_list = split_data(X, Y, attr_index, True)
+        # create sub-trees
+        for i in range(len(X_list)):
+            node.addChild(decision_tree(X_list[i], Y_list[i], num_attr, cat_attr))
+    else:
+        # attribute is categorical, perform split on every feature value
+        X_list, Y_list = split_data(X, Y, attr_index, False, cat_attr[attr_index])
+        # create sub-trees
+        for i in range(len(X_list)):
+            node.addChild(decision_tree(X_list[i], Y_list[i], num_attr, cat_attr))
+    # return the node created
+    return node    
+
 
 X, X_hot, Y = parse_csv("bank_train.csv")
-print(X_hot.shape)
+num_attr = set([0, 5, 9, 11, 12, 13, 14])
+cat_attr = {1: 12, 2: 4, 3: 4, 4: 3, 6: 3, 7: 3, 8: 3, 10: 12, 15: 4}
+tree = decision_tree(X, Y, num_attr, cat_attr)
